3a63b874233e75c80e93c6ea311caeda88d782e5
liang05l <3034586416@qq.com>
2026-02-16 02:25:21 +0800

    first
    only the test

---
+++ test.js
@@ -0,0 +1,17 @@
+// 算法助手规则示例
+{
+    "packageName": "com.llvmasml.malltacp",
+    "hooks": [
+        {
+            "className": "muhuaya.c6",
+            "methodName": "a",
+            "action": "return-true",
+            "params": ["android.app.Activity"]
+        },
+        {
+            "className": "android.app.AlertDialog",
+            "methodName": "show", 
+            "action": "return-null"
+        }
+    ]
+}
